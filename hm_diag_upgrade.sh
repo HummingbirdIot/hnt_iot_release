119c4609d066c50d389237aa5d0b98304a9ee0ca
hummingbirdiot <hummingbirdiot@outlook.com>
2021-12-07 15:17:34 +0800

    update: hm-diag upgrade

--- hm_diag_upgrade.sh
+++ hm_diag_upgrade.sh
@@ -25,11 +25,11 @@ function to_update() {
     echo "yes"
     return
   fi
-  
+
   now_v=`dpkg -s hm-diag | grep Version | grep -v - | cut -d ':' -f 2`
   now_v=${now_v// /}
 
-  if [ "$new_v" == "$now_v" ]; then 
+  if [ "$new_v" == "$now_v" ]; then
     echo "no"
     return
   else
@@ -41,6 +41,22 @@ function to_update() {
 function download_install() {
   down_url=`echo $latest_res | jq '.assets[3].browser_download_url'`
   down_url=${down_url//\"/}
+  if [ -f ".proxyconf"  ]; then
+    proxy_type=`cat .proxyconf | jq '.releaseFileProxy.type'`
+    proxy_type=${proxy_type//\"/}
+    proxy_url=`cat .proxyconf | jq '.releaseFileProxy.value'`
+    proxy_url=${proxy_url//\"/}
+    if [ $proxy_type == "urlPrefix" ] && [ "$proxy_url" != "null" ]; then
+      down_url="${proxy_url}${down_url}"
+      echo "use proxy type: ${proxy_type} , prefix url: ${proxy_url} , download url: ${down_url}"
+    elif [ $proxy_type == "mirror" ] && [ "$proxy_url" != "null" ]; then
+      path=`echo $down_url | cut -d/ -f4-`
+      down_url="$proxy_url$path"
+      echo "use proxy type: ${proxy_type} , prefix url: ${proxy_url} , download url: ${down_url}"
+    else
+      echo "wrong proxy config, proxy type: ${proxy_type}, value: ${proxy_url} , ignore it."
+    fi
+  fi
 
   ts=`date +%s`
   TEMP_DEB="/tmp/${deb_name}_${new_v}_$ts" &&
