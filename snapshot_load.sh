f19f498d4db7b909b4207edbb59e147c5f277fd6
hummingbirdiot <hummingbirdiot@outlook.com>
2021-12-15 18:18:15 +0800

    optimize: snapshot

--- snapshot_load.sh
+++ snapshot_load.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
-idFile=/tmp/.snapshot_load_id
+set -e
 
-if test -f "$idFile";then
-  echo "in progress"
-  return 101
+MC=hnt_iot_helium-miner_1 # miner container name
+ID_FILE=/tmp/.snapshot_load
+
+trap "rm -f $ID_FILE" EXIT
+
+fileName=$1
+if [ ! -f "$fileName" ]; then
+  echo "snapshot file $fileName not exist"
+  exit 1
 fi
 
-touch $idFile
-fileName=$1
+function loading() {
+  local s=`docker exec $MC ps`
+  if [ $? -gt 0 ]; then
+    echo "error $?"
+    return 0
+  fi
+  local res=`echo $s | grep "snapshot load"`
+  if [ -z "$res" ]; then
+    echo "no"
+  else
+    echo "yes"
+  fi
+}
 
-echo "load snapshot" $fileName
+if test -f "$ID_FILE";then
+  echo "in progress"
+  exit 101
+fi
+if [ `loading` == "yes" ];then
+  echo "load program running"
+  exit 101
+fi
+
+printf "file=$fileName\ntime=`date +%s`" > $ID_FILE
+echo "load snapshot $fileName"
 sudo docker exec hnt_iot_helium-miner_1 miner snapshot load $fileName
-
-rm -f $idFile
