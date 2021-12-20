0e20d65086cf606440993e5a728984590596d8d5
hummingbirdiot <hummingbirdiot@outlook.com>
2021-12-20 15:57:50 +0800

    update: hm-diag upgrade

--- hm_diag_upgrade.sh
+++ hm_diag_upgrade.sh
@@ -1,97 +1,21 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-deb_name=hm-diag
-url=https://api.github.com/repos/HummingbirdIot/hm-diag/releases/latest
+NAME=hm-diag
+PKG=${NAME}_linux_arm64.deb
 
-command -v jq > /dev/null || sudo apt-get install jq -y
+new_v=`dpkg-deb -W ./$PKG | cut -f2`
 
-latest_res=`curl -s $url`
-new_v=`echo $latest_res | jq '.tag_name'`
-new_v=${new_v//\"/}
+now_v=`dpkg -s ${NAME} | grep '^Status: deinstall' && echo "uninstalle" \
+    || dpkg -s ${NAME} | grep '^Version: ' | cut -d: -f2`
+now_v=${now_v// /}
 
-function is_installed() {
-  if systemctl list-units --full -all | grep -q hm-diag.service; then
-    echo "yes"
-  else
-    echo "no"
-  fi
-}
+if [ "$new_v" == "$now_v" ]; then
+  echo "$NAME is the latest version, give up upgrade"
+  exit 0
+fi
 
-function to_update() {
-  installed=`is_installed`
-  if [ "$installed" == "no" ]; then
-    echo "yes"
-    return
-  fi
-
-  now_v=`dpkg -s hm-diag | grep Version | grep -v - | cut -d ':' -f 2`
-  now_v=${now_v// /}
-
-  if [ "$new_v" == "$now_v" ]; then
-    echo "no"
-    return
-  else
-    echo "yes"
-    return
-  fi
-}
-
-function download_install() {
-  down_url=`echo $latest_res | jq '.assets[3].browser_download_url'`
-  down_url=${down_url//\"/}
-  if [ -f ".proxyconf"  ]; then
-    proxy_type=`cat .proxyconf | jq '.releaseFileProxy.type'`
-    proxy_type=${proxy_type//\"/}
-    proxy_url=`cat .proxyconf | jq '.releaseFileProxy.value'`
-    proxy_url=${proxy_url//\"/}
-    if [ $proxy_type == "urlPrefix" ] && [ "$proxy_url" != "null" ]; then
-      down_url="${proxy_url}${down_url}"
-      echo "use proxy type: ${proxy_type} , prefix url: ${proxy_url} , download url: ${down_url}"
-    elif [ $proxy_type == "mirror" ] && [ "$proxy_url" != "null" ]; then
-      path=`echo $down_url | cut -d/ -f4-`
-      down_url="$proxy_url$path"
-      echo "use proxy type: ${proxy_type} , prefix url: ${proxy_url} , download url: ${down_url}"
-    else
-      echo "wrong proxy config, proxy type: ${proxy_type}, value: ${proxy_url} , ignore it."
-    fi
-  fi
-
-  ts=`date +%s`
-  TEMP_DEB="/tmp/${deb_name}_${new_v}_$ts" &&
-  wget -O "$TEMP_DEB" $down_url &&
-  sudo dpkg -i "$TEMP_DEB"
-  rm -f "$TEMP_DEB"
-}
-
-function upgrade_hm_diag() {
-  to_update=`to_update`
-  if [[ "$to_update" != "yes" ]]; then
-    echo "$deb_name is the latest version, give up upgrade"
-  else
-    echo "to upgrade hm-diag ..."
-    download_install
-    echo "$deb_name upgrade done"
-  fi
-}
-
-function set_default_proxy() {
-  if [  -f ".proxyconf" ]; then
-    return
-  fi
-
-  cat << EOF > ./.proxyconf
-{
-  "releaseFileProxy": {
-    "type": "urlPrefix",
-    "value": "https://ghproxy.com/"
-  }
-}
-EOF
-
-  echo "set default github release proxy: `cat .proxyconf`"
-}
-
-set_default_proxy
-upgrade_hm_diag
+echo "to upgrade ${NAME} ..."
+sudo dpkg -i "$PKG" 
+echo "$NAME upgrade done"
