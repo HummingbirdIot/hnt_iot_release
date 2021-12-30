308bbac40ea919e8604aae06de39249c12f5ff47
hummingbirdiot <hummingbirdiot@example.com>
2021-12-30 11:54:54 +0800

    update: trigger hm-diag ota by version file

--- hm_diag_upgrade.sh
+++ hm_diag_upgrade.sh
@@ -2,45 +2,22 @@
 
 set -e
 
-deb_name=hm-diag
-url=https://api.github.com/repos/HummingbirdIot/hm-diag/releases/latest
+NAME=hm-diag
 
-command -v jq > /dev/null || sudo apt-get install jq -y
-
-latest_res=`curl -s $url`
-new_v=`echo $latest_res | jq '.tag_name'`
-new_v=${new_v//\"/}
-
-function is_installed() {
-  if systemctl list-units --full -all | grep -q hm-diag.service; then
-    echo "yes"
-  else
-    echo "no"
-  fi
+function now_version() {
+  local now_v=`dpkg -s ${NAME} | grep '^Status: deinstall' && echo "uninstalle" \
+      || dpkg -s ${NAME} | grep '^Version: ' | cut -d: -f2`
+  now_v=${now_v// /}
+  echo "$now_v"
 }
 
-function to_update() {
-  installed=`is_installed`
-  if [ "$installed" == "no" ]; then
-    echo "yes"
-    return
-  fi
-
-  now_v=`dpkg -s hm-diag | grep Version | grep -v - | cut -d ':' -f 2`
-  now_v=${now_v// /}
-
-  if [ "$new_v" == "$now_v" ]; then
-    echo "no"
-    return
-  else
-    echo "yes"
-    return
-  fi
+function new_version() {
+  cat ./.hm-diag-version
 }
 
 function download_install() {
-  down_url=`echo $latest_res | jq '.assets[3].browser_download_url'`
-  down_url=${down_url//\"/}
+  local new_v=`new_version`
+  local down_url="https://github.com/HummingbirdIot/hm-diag/releases/download/${new_v}/hm-diag_linux_arm64.deb" 
   if [ -f ".proxyconf"  ]; then
     proxy_type=`cat .proxyconf | jq '.releaseFileProxy.type'`
     proxy_type=${proxy_type//\"/}
@@ -59,20 +36,22 @@ function download_install() {
   fi
 
   ts=`date +%s`
-  TEMP_DEB="/tmp/${deb_name}_${new_v}_$ts" &&
+  TEMP_DEB="/tmp/${NAME}_${new_v}_$ts" &&
   wget -O "$TEMP_DEB" $down_url &&
   sudo dpkg -i "$TEMP_DEB"
   rm -f "$TEMP_DEB"
 }
 
 function upgrade_hm_diag() {
-  to_update=`to_update`
-  if [[ "$to_update" != "yes" ]]; then
-    echo "$deb_name is the latest version, give up upgrade"
+  local now_v=`now_version`
+  local new_v=`new_version`
+
+  if [ "$new_v" == "$now_v" ]; then
+    echo "$NAME is the latest version, give up upgrade"
   else
     echo "to upgrade hm-diag ..."
     download_install
-    echo "$deb_name upgrade done"
+    echo "$NAME upgrade done"
   fi
 }
 
