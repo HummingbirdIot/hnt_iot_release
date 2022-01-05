18e1dd6b36ecaf8faa0e2b13043c51c82db8b5da
hummingbirdiot <hummingbirdiot@example.com>
2022-01-05 17:40:59 +0800

    update: get miner log for json format

--- miner_log.sh
+++ miner_log.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -x
+
+command -v jq > /dev/null || sudo apt-get install jq -y
+
 # usage eg: bash miner_log.sh  '2022-01-05 13:36:15' '2022-01-05 14:07:15' 'JSON up'
 since="$1"
 until="$2"
 filter="$3"
 
-journalctl -o short-iso CONTAINER_NAME=hnt_iot_packet-forwarder_1 -S "$since" -U "$until" --no-pager | grep "$filter" | sed s/\\s.*[0-9]*]://
+journalctl -o json CONTAINER_NAME=hnt_iot_packet-forwarder_1 -S "$since" -U "$until" | grep "$filter" | jq -c '{message:.MESSAGE, time: ._SOURCE_REALTIME_TIMESTAMP}'
